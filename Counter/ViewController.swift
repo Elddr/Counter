53e70d01bdaf3bdc80d43ad43c82621bbfd68418
Elddr <eldar.doroshenko@mail.ru>
2023-08-14 18:50:58 +0300

    Выполнил базовую задачу по 3 спринту

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var CounterNum = 0
+    @IBOutlet weak var CounterText: UILabel!
+    
+    @IBAction func CounterButton(_ sender: Any) {
+        CounterNum += 1
+        CounterText.text = String(CounterNum)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
